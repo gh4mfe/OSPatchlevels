d6b14d414626a843b0039b84d739665240ca5a91
Marco Feuerstein (Clientadministrator) <admc.feuerstein@nb.edeka.net>
2022-07-18 16:58:47 +0200

    JSonogv

--- JsonGridView.ps1
+++ JsonGridView.ps1
@@ -6,4 +6,20 @@ foreach ($Item in $AllFiles) {
     $Results += Get-Content $Item.FullName -Raw | ConvertFrom-Json
 }
 
-$Results | where-object {$_.OSBuild -ne $null -and $_.OSBuild -notlike "*not*"} |Sort-Object KBFULL -Unique | ogv
+$resultplus = $Results | select *,@{Name='Date';Expression={(get-date -Year ($_.Title).SubString(0,4) -Month ($_.Title).SubString(5,2) -Day 1)}} | select *,@{Name='Build';Expression={([version]$_.OSBuild).Build}}  | select *,@{Name='Revision';Expression={([version]$_.OSBuild).Revision}}
+
+$resultplus | Sort-Object Revision | ogv
+
+$Results  | Sort-Object OSBuild -Unique | Export-Csv -NoClobber -NoTypeInformation .\result-osbuild.csv
+
+
+where-object {$_.OSBuild -ne $null -and $_.OSBuild -notlike "*not*"}
+|Sort-Object KBFULL -Unique 
+
+[version]"2.0.50727.5446"
+
+($_.Title).SubString(0,4)
+($_.Title).SubString(5,2)
+
+(get-date -Year ($_.Title).SubString(0,4) -Month ($_.Title).SubString(5,2) -Day 1)
+Get-Date -Month 
